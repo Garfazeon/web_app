ef5b4c83fad6e76329084835648ca928c9a26ada
Garfazeon <103832233+Garfazeon@users.noreply.github.com>
2025-06-07 05:26:08 +1000

    profile.php

--- profile.php
+++ profile.php
@@ -16,9 +16,6 @@ $username = htmlspecialchars($_COOKIE['User']);
 <body class="bg-light d-flex flex-column justify-content-center align-items-center vh-100">
     <div class="text-center">
         <h1>Привет, <?= $username ?>!</h1>
-        <form action="logout.php" method="POST" class="mt-4">
-            <button class="btn btn-danger" type="submit">Выйти</button>
-        </form>
     </div>
 </body>
 </html>
